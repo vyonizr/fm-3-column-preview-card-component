32f8d6bb33d0333f0644aadedbac94f2ec8b2751
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-01-10 20:19:12 +0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Component, For } from 'solid-js'
 
 import Card from './components/Card';
+import Footer from './components/Footer';
 import { CONTENTS } from './constants';
 
 const App: Component = () => {
   return (
-    <main class="lg:items-center; w-max overflow-hidden rounded-xl text-white lg:grid lg:grid-flow-col">
-      <For each={CONTENTS}>{(content) => <Card content={content} />}</For>
-    </main>
+    <>
+      <main class="lg:items-center; w-max overflow-hidden rounded-xl text-white lg:grid lg:grid-flow-col">
+        <For each={CONTENTS}>{(content) => <Card content={content} />}</For>
+      </main>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import { Component } from 'solid-js';
+
+const Footer: Component = () => {
+  return (
+    <footer class="attribution">
+      Challenge by{' '}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{' '}
+      <a href="https://www.frontendmentor.io/profile/vyonizr" target="_blank">
+        vyonizr
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
