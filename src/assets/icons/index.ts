a2d969bff60ce5b3c744e1bb286ce5615b732ffa
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-01-10 17:28:10 +0700

    initial commit

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import iconLuxury from './icon-luxury.svg'
+import iconSedans from './icon-sedans.svg'
+import iconSuvs from './icon-suvs.svg'
+
+export { iconLuxury, iconSedans, iconSuvs }
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: [
+        'Lexend Deca',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      display: ['Big Shoulders Display', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        white: 'hsla(0, 0%, 95%, 1)',
+      },
+    },
+  },
+  plugins: [],
+}
